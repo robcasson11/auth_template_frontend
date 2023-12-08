76daffb63480a79fb69523b515d7b692bfe57161
rob <robertcasson11@gmail.com>
2023-12-08 16:06:43 +0000

    Ready for deploy

--- src/components/login.js
+++ src/components/login.js
@@ -37,8 +37,6 @@ const Login = () => {
           withCredentials: true,
         }
       );
-      console.log(JSON.stringify(response?.data));
-      //console.log(JSON.stringify(response));
       const accessToken = response?.data?.accessToken;
       const roles = response?.data?.roles;
       setAuth({ user, pwd, roles, accessToken });

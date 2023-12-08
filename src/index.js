54e306c3c02f8802f150d6f03325347cb959b815
rob <robertcasson11@gmail.com>
2023-12-08 16:16:34 +0000

    Read to deploy

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthProvider";
 import { LogoutProvider } from "./context/LogoutContext";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
